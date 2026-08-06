1e199454b6b29c2dd1e35b84e4aa4ecb1f8ab787
TranSon2707 <trantienson2707@gmail.com>
2026-08-06 19:18:34 +0700

    minor changes to readme and add app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { ClassificationModule } from './classification/classification.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [],
+  imports: [ClassificationModule, StatisticsModule],
   controllers: [AppController],
   providers: [AppService,PrismaService],
 })
